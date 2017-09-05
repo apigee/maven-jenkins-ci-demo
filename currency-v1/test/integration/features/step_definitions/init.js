d4307ebea01af7cff3b8f3731cd9becb667f92e7
Ozan Seymen <oseymen@gmail.com>
2017-09-04 22:37:18 -0230

    set jenkins docker version to v2.62

--- currency-v1/test/integration/features/step_definitions/init.js
+++ currency-v1/test/integration/features/step_definitions/init.js
@@ -7,6 +7,9 @@ var config = require('../../test-config.json');
 console.log('currency api: [' + config.currencyApi.domain + ', ' + config.currencyApi.basepath + ']');
 
 module.exports = function() {
+
+    this.setDefaultTimeout(60 * 1000);
+
 	// cleanup before every scenario
 	this.Before(function(scenario, callback) {
 		this.apickli = new apickli.Apickli('https',
