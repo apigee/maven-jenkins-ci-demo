60bca69e33d831975cf0317e96196beeed9cdbf6
dknezic <dknezic@google.com>
2019-02-07 17:33:48 +0000

    Update url

--- currency-v1/test/integration/features/step_definitions/rates.js
+++ currency-v1/test/integration/features/step_definitions/rates.js
@@ -20,7 +20,7 @@ var Promise = require('bluebird');
 var assertSuccessfulApiResponse = function(apickli) {
 	return new Promise(function(resolve, reject) {
 		if (!apickli.scenarioVariables.isResponseSuccessful) {
-			var assertion = apickli.assertResponseCode("200");
+			var assertion = apickli.assertResponseCode("200") || apickli.assertResponseCode("304");
 			if (!assertion.success) {
 				return reject(assertion);
 			}
@@ -42,23 +42,23 @@ var assertSuccessfulApiResponse = function(apickli) {
 module.exports = function() {
 
 	this.When(/^I request all exchange rates with default values$/, function(callback) {
-		this.apickli.get('/rates', callback);
+		this.apickli.get('/', callback);
 	});
 
 	this.When(/^I request all exchange rates with (.{3}) as the base currency$/, function(base, callback) {
 		this.apickli.queryParameters.base = base;
-		this.apickli.get('/rates', callback);
+		this.apickli.get('/', callback);
 	});
 
 	this.When(/^I request all exchange rates for (.*)$/, function(date, callback) {
 		this.apickli.queryParameters.date = date;
-		this.apickli.get('/rates', callback);
+		this.apickli.get('/', callback);
 	});
 
 	this.When(/^I request all exchange rates with (.{3}) as the base currency for (.*)$/, function(base, date, callback) {
 		this.apickli.queryParameters.base = base;
 		this.apickli.queryParameters.date = date;
-		this.apickli.get('/rates', callback);
+		this.apickli.get('/', callback);
 	});
 
 	this.Then(/^I should see (.*) as the base currency$/, function(base, callback) {
