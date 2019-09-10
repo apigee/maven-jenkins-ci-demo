f96e37f079f95c3cdead7ee70bbde9516503975a
igalonso <igalonso@gmail.com>
2019-09-10 16:52:18 +0200

    Improvement to ConfigureTargetRequest Script

--- currency-v1/apiproxy/resources/jsc/ConfigureTargetRequest.js
+++ currency-v1/apiproxy/resources/jsc/ConfigureTargetRequest.js
@@ -13,6 +13,4 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
 */
-context.setVariable('target.copy.pathsuffix', false);
-
-somethigngfgh 
+context.setVariable('target.copy.pathsuffix', false); 
